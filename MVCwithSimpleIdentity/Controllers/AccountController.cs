a76dcb178a348df5623a4a3cd750e659d7f40d78
DESKTOP-EPN9CP6\thebe <oleh.knyaz@gmail.com>
2019-02-27 16:39:02 +0200

    add metthods

--- MVCwithSimpleIdentity/Controllers/AccountController.cs
+++ MVCwithSimpleIdentity/Controllers/AccountController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNet.Identity.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using MVCwithSimpleIdentity.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -21,30 +23,82 @@ namespace MVCwithSimpleIdentity.Controllers
             get => HttpContext.GetOwinContext().Authentication;
         }
 
-        [HttpPost]
-        public ActionResult Register(RegisterModel model)
-        {
-           //TODO: add method body!!!
-            return HttpNotFound();
-        }
-
-        [HttpPost]
-        public ActionResult Login(LoginModel model, string returnUrl)
-        {
-            //TODO: add method body!!!
-            return HttpNotFound();
-        }
-
-
-
-
-
-
-
-        // GET: Account
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Register()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    BirthDay = model.BirthDay,
+                    UserName = model.Email,
+                    Name = model.Name,
+                    Surname = model.Surname,
+                    Email = model.Email
+                };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await UserManager.FindAsync(model.Email, model.Password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Login or Password is invalid!");
+                }
+                else
+                {
+                    var identityUser = await UserManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    AuthManager.SignOut();
+                    AuthManager.SignIn(new AuthenticationProperties { IsPersistent = true }, identityUser);
+                    if (String.IsNullOrEmpty(returnUrl))
+                    {
+                        RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(returnUrl);
+                }
+            }
+
+            ViewBag.returnUrl = returnUrl;
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            AuthManager.SignOut();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
